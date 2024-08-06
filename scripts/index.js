89b7b60ea7875e39c636e4f78f85bfef2117ee7c
Eduardo Costa <eduardo.costa@docente.senai.br>
2024-08-06 11:28:24 -0300

    Inicializa o projeto com a home

---
+++ scripts/index.js
@@ -0,0 +1,4 @@
+function mostraMenu() {
+    document.getElementById("navbar").classList.toggle("mostra-menu");
+    document.getElementById("menu-hamburger").classList.toggle("menu-hamburger-rotate");
+}
